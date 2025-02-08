6b3b7a966608a38411d4c34e12510b1ac8e43c53
Anna Clara <almeida.annaclara@outlook.com>
2025-02-08 15:50:09 -0300

    Ex4

---
+++ Capitulo2/Parte 2/Ex4.py
@@ -0,0 +1,12 @@
+distancia = float(input("Qual a distancia da viagem em km?"))
+if distancia < 200.00:
+    preco = 0.50*distancia
+    print("Sua passagem custa R$", preco)
+if distancia > 200.00:
+    preco = 0.45*distancia
+    print("Sua passagem custa R$", preco)
+if distancia == 200.00:
+    preco = 0.50200*distancia
+    print("Sua passagem custa R$", preco)
+else:
+    print("distancia inválida")
