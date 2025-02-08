34f962a7e12a708830103052cd03716ab09e10ac
Anna Clara <almeida.annaclara@outlook.com>
2025-02-08 16:20:56 -0300

    Ex6.correção

--- Capitulo2/Parte 2/Ex6.py
+++ Capitulo2/Parte 2/Ex6.py
@@ -1,6 +1,6 @@
 import math
 num = float(input("Entre com um numero decimal."))
-print(math.sqrt(num))
-print(math.ceil(num))
-print(math.floor(num))
-print(math.trunc(num))
+print("Sua raiz quadrada é", math.sqrt(num))
+print("Arredondado para cima", math.ceil(num))
+print("Arredondado para baixo",math.floor(num))
+print("Sua parte inteira é",math.trunc(num))
