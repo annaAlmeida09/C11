628faf429b6fd45811381f831903dc7e34ea52c2
Anna Clara <almeida.annaclara@outlook.com>
2025-02-12 19:59:48 -0300

    Criação capítulo 3

---
+++ Capitulo3/AulaColecoesC11.py
@@ -0,0 +1,7 @@
+nomes = ('Goku', 'Vegeta', 'Trunks', 'Gohan') # tupla
+print(nomes)
+# SLICING DE DADOS
+print(nomes[0]) # primeiro nome
+# no Python, o primeiro indice de uma colecao é INCLUSIVE e o seguinte é EXCLUSIVE
+print(nomes[:2]) # os dois primeiros nomes (é como se pegasse a posição 0 e 1, a 2 é EXCLUSIVE não pega a 2)
+print(nomes[2:]) # os dois ultimos nomes (posição 2 INCLUSIVE até o final da tupla)
