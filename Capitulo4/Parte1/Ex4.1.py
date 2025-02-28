aaa0b0667b63c6d517de2d91b89d6dd060457058
Anna Clara <almeida.annaclara@outlook.com>
2025-02-28 20:28:33 -0300

    Exercicio 1 capitulo 4

---
+++ Capitulo4/Parte1/Ex4.1.py
@@ -0,0 +1,17 @@
+#importando o numpy
+import numpy as np
+
+#criando arrays
+array1 = np.ones(8, dtype=int)
+array2 = np.random.randint(0, 9, [8]) 
+
+array3 = array1 + array2
+
+soma = np.sum(array3)
+print(soma)
+if soma >= 40:
+    array3 = np.reshape(array3, (8,1)) 
+else:
+    array3 = np.reshape(array3, (1,8)) 
+
+print(array3)
